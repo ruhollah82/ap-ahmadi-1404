3bea52948d4e48f11034eef757a476249c9a1eb1
Rhuhollah <0robena0@gmail.com>
2025-02-26 12:54:48 +0330

    first commit1

--- ap_1/ap_1_0.cpp
+++ ap_1/ap_1_0.cpp
@@ -2,6 +2,50 @@
 
 using namespace std;
 
+struct smartPhone
+{
+    string name;
+    int internalStorage;
+    string color;
+};
+
+void display(smartPhone MyPhone)
+{
+    cout << "Name: " << MyPhone.name << endl;
+    cout << "Internal Storage : " << MyPhone.internalStorage << endl;
+    cout << "Color : " << MyPhone.color << endl;
+    cout << "------------------------------" << endl;
+}
+
+smartPhone upgrade(smartPhone MyPhone)
+{
+    MyPhone.internalStorage = 1024;
+    return MyPhone;
+}
+
 int main()
 {
+    smartPhone p1;
+    p1.name = "iphon 12";
+    p1.internalStorage = 256;
+    p1.color = "black";
+
+    smartPhone p2;
+    p2.name = "galaxy s24";
+    p2.internalStorage = 512;
+    p2.color = "white";
+
+    display(p1);
+    display(p2);
+
+    smartPhone A[10];
+
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> A[i].name;
+    }
+
+    p1 = upgrade(p1);
+
+    display(p1);
 }
